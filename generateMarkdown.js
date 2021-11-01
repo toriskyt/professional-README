ebe3bf163268f60a6c30ec2aade44eb0dd00ac83
Tracy Torisky <mommers1@verizon.net>
2021-11-01 17:51:37 -0400

    added new code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,19 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {license.badge}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {license.link}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {license.section}
 
-// TODO: Create a function to generate markdown for README
+// DONE: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+# Description
+
+// ${data.Description}
+
+# Table of Contents
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Licence](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](*tests)
+
+* [Questions](*questions)
+
+# Installation
+
+// ${data.Installation}
+
+# Usage
+
+// ${data.Usage}
+
+# License
+
+// ${data.License}
+
+# Contributing
+
+// ${data.Contribution}
+
+#Tests
+
+// ${data.Test}
+
+#Questions
+Let me know if you have any questions.
+
+Find my GitHub repository at ${data.UserName} or use my email address at ${data.Email}
 `;
 }
 
--- index.js
+++ index.js
@@ -2,70 +2,86 @@
 const inquirer = require('inquirer');
 // DONE: brought in file system from nodejs library
 const fs = require("fs");
+// DONE: create a link to the markdown file
+const generateMarkdown = require("./generateMarkdown");
 // : DONE:  Create an array of questions for user input
-const questions = [
-    {
-        type: "input",
-        message: "Title of README",
-        name: "title",
-    },
+const questions =
+    [
+        {
+            type: "input",
+            message: "Title of README",
+            name: "title",
+        },
 
-    {
-        type: "input",
-        message: "Description of Project",
-        name: "description",
-    },
+        {
+            type: "input",
+            message: "Description of Project",
+            name: "description",
+        },
 
-    {
-        type: "input",
-        message: "Table of Contents",
-        name: "contents",
-    },
+        {
+            type: "input",
+            message: "Table of Contents",
+            name: "contents",
+        },
 
-    {
-        type: "input",
-        message: "Installation",
-        name: "install",
-    },
+        {
+            type: "input",
+            message: "Installation",
+            name: "install",
+        },
 
-    {
-        type: "input",
-        message: "Usage",
-        name: "usage",
-    },
+        {
+            type: "input",
+            message: "Usage",
+            name: "usage",
+        },
 
-    {
-        type: "input",
-        message: "License",
-        name: "license",
-    },
+        {
+            type: "input",
+            message: "License",
+            name: "license",
+        },
 
-    {
-        type: "input",
-        message: "Contributing",
-        name: "contribute",
-    },
+        {
+            type: "input",
+            message: "Contributing",
+            name: "contribute",
+        },
 
-    {
-        type: "input",
-        message: "Tests",
-        name: "test",
-    },
+        {
+            type: "input",
+            message: "Tests",
+            name: "test",
+        },
 
-    {
-        type: "input",
-        message: "Questions",
-        name: "question",
-    }
-];
+        {
+            type: "input",
+            message: "Questions",
+            name: "question",
+        }
+    ];
 
 console.log(questions);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    fs.writeFile(filename, data, function(err){
+        if (err) {
+            return console.log(err);
+        }
+        console.log ("Success!");
+    })
+ }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile("professional-README", generateMarkdown(data));
+    })
+}
 
 // Function call to initialize app
 init();
